ce22a4de75db275d8b655e0648de6a7ee892f988
MonkiNam <90295325+MonkiNam@users.noreply.github.com>
2022-12-25 23:13:23 +0700

    [BE] Add Repository + Add Category service demo (#5)

---
+++ BE_Homnayangi/Modules/CategoryModule/Interface/ICategoryService.cs
@@ -0,0 +1,21 @@
+﻿using Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace BE_Homnayangi.Modules.CategoryModule.Interface
+{
+    public interface ICategoryService
+    {
+        public Task AddNewCategory(Category newCategory);
+        public Task UpdateCategory(Category categoryUpdate);
+        public Task DeleteCategory(Guid? ID);
+        public ICollection<Category> GetAll();
+        public ICollection<Category> GetCategoriesBy(Expression<Func<Category, bool>> filter = null,
+            Func<IQueryable<Category>, ICollection<Category>> options = null,
+            string includeProperties = null);
+        public Category GetCategoryByID(Guid? cateID);
+    }
+}
---
+++ Repository/Repository/CategoryRepository/CategoryRepository.cs
@@ -0,0 +1,46 @@
+﻿using Library.DataAccess;
+using Library.Models;
+using Microsoft.EntityFrameworkCore;
+using Repository.Repository.CategoryRepository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Repository.CategoryRepository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private readonly HomnayangiContext _db;
+
+        public CategoryRepository(HomnayangiContext db) : base(db)
+        {
+            _db = db;
+        }
+        public ICollection<Category> GetCategoriesBy(
+            Expression<Func<Category, bool>> filter = null,
+            Func<IQueryable<Category>, ICollection<Category>> options = null,
+            string includeProperties = null
+        )
+        {
+            IQueryable<Category> query = DbSet;
+
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            if (includeProperties != null)
+            {
+                foreach (var includeProp in includeProperties.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(includeProp);
+                }
+            }
+
+            return options != null ? options(query).ToList() : query.ToList();
+        }
+    }
+}
