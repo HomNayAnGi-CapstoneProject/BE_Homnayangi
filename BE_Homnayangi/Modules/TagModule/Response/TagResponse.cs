05d2e743fb20b631f9839893c48855fafbbcd650
Peter Võ <vanphuong0606@gmail.com>
2023-01-09 19:55:31 +0700

    [BE]: Get tags by categoryId

---
+++ BE_Homnayangi/Modules/TagModule/Response/TagResponse.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace BE_Homnayangi.Modules.TagModule.Response
+{
+    public class TagResponse
+    {
+        public Guid TagId { get; set; }
+        public string Name { get; set; }
+    }
+}
