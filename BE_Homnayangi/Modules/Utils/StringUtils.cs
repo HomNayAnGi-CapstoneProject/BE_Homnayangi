1c49cc62b65ec0011bd056c838e8a80e180bd9fd
vuhoangdinhphuc <phucvhd@gmail.com>
2023-01-29 00:06:44 +0700

    migrate ingredient cru services

---
+++ BE_Homnayangi/Modules/Utils/StringUtils.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BE_Homnayangi.Modules.Utils
+{
+    public static class StringUtils
+    {
+        public static string CompressContents(ICollection<string> listContents)
+        {
+            try
+            {
+                var result = "";
+                listContents.ToList().ForEach(c =>  result += ";"+c);
+                return result;
+            }
+            catch
+            {
+                throw new Exception("Compress contents fail");
+            }
+        }
+        public static ICollection<string> ExtractContents(string listContents)
+        {
+            try
+            {
+                var result = listContents.Split(";").ToList();
+                return result;
+            }
+            catch
+            {
+                throw new Exception("Extract contents fail");
+            }
+        }
+    }
+}
+
