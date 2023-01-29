2e85ea6fd88ce211e8db336249df174210efed81
MonkiNam <monkeynam208@gmail.com>
2023-01-29 21:38:38 +0700

    [BE] Hot Fix Ingredient

--- BE_Homnayangi/Modules/IngredientModule/Request/UpdatedIngredientRequest.cs
+++ BE_Homnayangi/Modules/IngredientModule/Request/UpdatedIngredientRequest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace BE_Homnayangi.Modules.IngredientModule.Request
 {
@@ -8,6 +9,7 @@ namespace BE_Homnayangi.Modules.IngredientModule.Request
         public string Name { get; set; }
         public string Description { get; set; }
         public string Quantitative { get; set; }
+        public ICollection<string> ListImage { get; set; }
         public string Picture { get; set; }
         public decimal Price { get; set; }
         public Guid TypeId { get; set; }
