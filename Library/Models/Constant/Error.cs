0a76f896d93d87ff5fe42f198926f44acc426671
vuhoangdinhphuc <phucvhd@gmail.com>
2023-02-15 20:49:36 +0700

    Update Error.cs

--- Library/Models/Constant/Error.cs
+++ Library/Models/Constant/Error.cs
@@ -3,11 +3,12 @@ namespace Library.Models.Constant
 {
     public static class Error
     {
+        #region User error message
         public static class UserError
         {
             public readonly static string USER_EXISTED = "Username is already existed";
         }
-        
+        #endregion
     }
 }
 
