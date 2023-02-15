0370323ab8a2b2b29682c389b14bc973db8c795b
vuhoangdinhphuc <phucvhd@gmail.com>
2023-02-15 11:09:42 +0700

    update Register

---
+++ Library/Models/Constant/Error.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Library.Models.Constant
+{
+    public static class Error
+    {
+        public static class UserError
+        {
+            public readonly static string USER_EXISTED = "Username is already existed";
+        }
+        
+    }
+}
+
