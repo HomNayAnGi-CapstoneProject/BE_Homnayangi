821c7376454ca16e0003780855450b1c7d2691c7
Minh Hoàng Trần <minhtrn0105@gmail.com>
2023-03-12 15:37:18 +0700

    Change gender type

---
+++ Library/Models/Enum/GenderEnum.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Models.Enum
+{
+    public class GenderEnum
+    {
+        public enum Gender
+        {
+            MALE = 0,
+            FEMALE = 1,
+            OTHER = 2,
+        }
+    }
+}
