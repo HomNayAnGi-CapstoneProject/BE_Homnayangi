587645b032cbab4e233c9f1d6532ed8e7661df3f
Minh Hoàng Trần <minhtrn0105@gmail.com>
2023-03-24 11:59:03 +0700

    Update BadgeJob

--- BE_Homnayangi/Ultils/Quartz/BadgeJob.cs
+++ BE_Homnayangi/Ultils/Quartz/BadgeJob.cs
@@ -3,6 +3,7 @@ using BE_Homnayangi.Modules.AdminModules.BadgeConditionModule.Interface;
 using BE_Homnayangi.Modules.BadgeModule.Interface;
 using BE_Homnayangi.Modules.CustomerBadgeModule.Interface;
 using BE_Homnayangi.Modules.CustomerModule.Interface;
+using BE_Homnayangi.Modules.OrderModule.Interface;
 using Library.Models;
 using Quartz;
 using System;
@@ -22,17 +23,19 @@ namespace BE_Homnayangi.Ultils.Quartz
         private readonly IAccomplishmentRepository _accomplishmenttRepository;
         private readonly IBadgeConditionRepository _badgeConditionRepository;
         private readonly ICustomerBadgeRepository _customerBadgeRepository;
-        public BadgeJob(ICustomerRepository customerRepository, IBadgeRepository badgeRepository, IAccomplishmentRepository accomplishmenttRepository, IBadgeConditionRepository badgeConditionRepository, ICustomerBadgeRepository customerBadgeRepository)
+        private readonly IOrderRepository _orderRepository;
+        public BadgeJob(ICustomerRepository customerRepository, IBadgeRepository badgeRepository, IAccomplishmentRepository accomplishmenttRepository, IBadgeConditionRepository badgeConditionRepository, ICustomerBadgeRepository customerBadgeRepository, IOrderRepository orderRepository)
         {
             _customerRepository = customerRepository;
             _badgeRepository = badgeRepository;
             _accomplishmenttRepository = accomplishmenttRepository;
             _badgeConditionRepository = badgeConditionRepository;
             _customerBadgeRepository = customerBadgeRepository;
+            _orderRepository = orderRepository;
         }
         public async Task Execute(IJobExecutionContext context)
         {
-            Console.WriteLine("Hello from Quartz hê hê");
+            Console.WriteLine("Hello from Quartz");
             await BadgeCondition();
 
         }
@@ -40,7 +43,21 @@ namespace BE_Homnayangi.Ultils.Quartz
         {
             var customers = await _customerRepository.GetAll();
             var accomplishmentsList = await _accomplishmenttRepository.GetAll();
-            customers = customers.Join(accomplishmentsList, x => x.CustomerId, y => y.AuthorId, (x, y) => x).ToList();
+            var ordersList = await _orderRepository.GetAll();
+            customers = customers
+         .GroupJoin(accomplishmentsList,
+            x => x.CustomerId,
+            y => y.AuthorId,
+             (x, y) => new { Customer = x, Accomplishments = y.DefaultIfEmpty() })
+         .SelectMany(x => x.Accomplishments
+             .Select(y => new { Customer = x.Customer, Accomplishment = y }))
+         .GroupJoin(ordersList,
+            x => x.Customer.CustomerId,
+             y => y.CustomerId,
+             (x, y) => new { CustomerWithAccomplishment = x, Orders = y.DefaultIfEmpty() })
+         .SelectMany(x => x.Orders
+             .Select(y => x.CustomerWithAccomplishment.Customer))
+         .ToList();
             foreach (Customer customer in customers)
             {
                 int accomplishments = customer.Accomplishments.Count();
