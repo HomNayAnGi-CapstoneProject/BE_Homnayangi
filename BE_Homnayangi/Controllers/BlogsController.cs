eb42f71666fcfa5978986aa29cc7211b4756f530
MonkiNam <monkeynam208@gmail.com>
2023-03-27 18:38:58 +0700

    [BE] Hot fix

--- BE_Homnayangi/Controllers/BlogsController.cs
+++ BE_Homnayangi/Controllers/BlogsController.cs
@@ -330,7 +330,7 @@ namespace BE_Homnayangi.Controllers
                 {
                     return new JsonResult(new
                     {
-                        status = "failed"
+                        status = "Some fields are null"
                     });
                 }
             }
