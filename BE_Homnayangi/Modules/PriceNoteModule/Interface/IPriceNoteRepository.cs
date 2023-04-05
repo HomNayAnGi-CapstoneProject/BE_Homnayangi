452b08ae48a790a0b969e401a93174f63b015119
Peter Võ <vanphuong0606@gmail.com>
2023-04-05 20:48:50 +0700

    [BE]: Add logic price note (create-update ingre)

---
+++ BE_Homnayangi/Modules/PriceNoteModule/Interface/IPriceNoteRepository.cs
@@ -0,0 +1,19 @@
+﻿using Library.Models;
+using Repository.Repository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace BE_Homnayangi.Modules.PriceNoteModule.Interface
+{
+    public interface IPriceNoteRepository: IRepository<PriceNote>
+    {
+        public Task<ICollection<PriceNote>> GetPriceNotesBy(
+           Expression<Func<PriceNote, bool>> filter = null,
+           Func<IQueryable<PriceNote>, ICollection<PriceNote>> options = null,
+           string includeProperties = null
+       );
+    }
+}
