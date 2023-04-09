2d1d0cf5dc5b6d88e28c1ba2eabc71629f3aca99
Minh Hoàng Trần <minhtrn0105@gmail.com>
2023-04-10 00:18:54 +0700

    [Hotfix] Update create user field

--- BE_Homnayangi/Modules/UserModule/Request/CreateUser.cs
+++ BE_Homnayangi/Modules/UserModule/Request/CreateUser.cs
@@ -6,8 +6,5 @@ namespace BE_Homnayangi.Modules.UserModule.Request
     {
         [Required(ErrorMessage = "Displayname is required")]
         public string Displayname { get; set; }
-        
-        [Required(ErrorMessage = "Avatar is required")]
-        public string Avatar { get; set; }
     }
 }
